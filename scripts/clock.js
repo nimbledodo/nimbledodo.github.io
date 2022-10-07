3ad6afab653bf61633667857168708a15b7159f6
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-07 22:22:24 +0900

    add clock functionality

---
+++ scripts/clock.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector(".clock span");
+
+function setClock() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = now.getSeconds();
+  clock.innerText = `${hours}:${minutes}`;
+  return seconds;
+}
+
+//Initially refresh every one second
+const initSec = setClock();
+let timerId = setInterval(setClock, 1000);
+setTimeout(() => {
+  clearInterval(timerId);
+}, (60 - initSec) * 1000);
+//Afterward refresh every ten seconds
+setInterval(setClock, 1000 * 10);
