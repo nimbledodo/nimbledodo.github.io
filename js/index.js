2224be5b04f80b081a5d211eda1413f6ebcbf67f
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-07 23:11:24 +0900

    Add random background image functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 const body = document.querySelector("body");
+const nBgImg = 12; // total number of background images
+const bgImgFolder = "./img/";
+const bgImgPrefix = "bg";
+const bgImgExt = "jpg";
+
+bgImgFileName = `${bgImgFolder}${bgImgPrefix}${Math.floor(
+  Math.random() * nBgImg
+)}.${bgImgExt}`;
+console.log(bgImgFileName);
+
+body.style.backgroundImage = "url('" + bgImgFileName + "')";
+// body.style.backgroundImage = "url('./img/bg1.jpg')";
