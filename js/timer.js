5cb29cdb1f56e3a6590fcf99faa9aed8cee8fa27
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-09 19:54:06 +0900

    timer works perfectly w/o canvas
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- js/timer.js
+++ js/timer.js
@@ -4,6 +4,9 @@ const bAddSec = document.querySelector("#add-sec");
 const bSubSec = document.querySelector("#sub-sec");
 const timeLeft = document.querySelector("#time-left");
 const bStartStop = document.querySelector("#timer i");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+let time = 0;
 
 const START_CLASS_NAME = "fa-play";
 const PAUSE_CLASS_NAME = "fa-pause";
@@ -11,7 +14,7 @@ const PAUSE_CLASS_NAME = "fa-pause";
 function addMin(e) {
   e.preventDefault();
   const curClassName = bStartStop.classList[1];
-  if (curClassName !== PAUSE_CLASS_NAME) {
+  if (curClassName !== PAUSE_CLASS_NAME && time === 0) {
     const lastTimeLeft = timeLeft.innerText;
     let lastMin = parseInt(lastTimeLeft.substr(0, 2));
     if (lastMin < 60) {
@@ -27,7 +30,7 @@ function addMin(e) {
 function subMin(e) {
   e.preventDefault();
   const curClassName = bStartStop.classList[1];
-  if (curClassName !== PAUSE_CLASS_NAME) {
+  if (curClassName !== PAUSE_CLASS_NAME && time === 0) {
     const lastTimeLeft = timeLeft.innerText;
     let lastMin = parseInt(lastTimeLeft.substr(0, 2));
     if (lastMin > 0) {
@@ -43,7 +46,7 @@ function subMin(e) {
 function addSec(e) {
   e.preventDefault();
   const curClassName = bStartStop.classList[1];
-  if (curClassName !== PAUSE_CLASS_NAME) {
+  if (curClassName !== PAUSE_CLASS_NAME && time === 0) {
     const lastTimeLeft = timeLeft.innerText;
     let lastSec = parseInt(lastTimeLeft.substr(3, 2));
     if (lastSec < 60) {
@@ -57,7 +60,7 @@ function addSec(e) {
 function subSec(e) {
   e.preventDefault();
   const curClassName = bStartStop.classList[1];
-  if (curClassName !== PAUSE_CLASS_NAME) {
+  if (curClassName !== PAUSE_CLASS_NAME && time === 0) {
     const lastTimeLeft = timeLeft.innerText;
     let lastSec = parseInt(lastTimeLeft.substr(3, 2));
     if (lastSec > 0) {
@@ -79,7 +82,7 @@ function onITagClick(e) {
       const timeLeftText = timeLeft.innerText;
       const min = parseInt(timeLeftText.substr(0, 2));
       const sec = parseInt(timeLeftText.substr(3, 2));
-      let time = min * 60 + sec;
+      time = min * 60 + sec;
       time--;
       if (bStartStop.classList[1] === START_CLASS_NAME) {
         clearInterval(timerId);
@@ -109,3 +112,9 @@ bSubMin.addEventListener("click", subMin);
 bAddSec.addEventListener("click", addSec);
 bSubSec.addEventListener("click", subSec);
 bStartStop.addEventListener("click", onITagClick);
+
+// ctx.beginPath();
+// ctx.arc(100, 100, 100, -Math.PI, 2 * Math.PI, false);
+// ctx.strokeStyle = "rgba(255, 255, 255, 0.5)";
+// ctx.lineWidth = 2;
+// ctx.stroke();
