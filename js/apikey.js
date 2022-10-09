58c801fd8b63efdbc47ee3a64262933faf529d96
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-09 10:59:07 +0900

    finished css for search
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

---
+++ js/apikey.js
@@ -0,0 +1,3 @@
+const config = {
+  apikey: "f899f6a9ec558ddfe8fad14425c8c403",
+};
--- js/search.js
+++ js/search.js
@@ -1,8 +1,11 @@
 const searchForm = document.querySelector("#search");
 const searchInput = searchForm.querySelector("input");
 const searchItag = document.querySelector("#icon");
-const mouseOn = false;
+let mouseOn = false;
+let clicked = false;
 const ICON_CLASS_NAME = "fa-google";
+const FOCUS_BORDER_BOTTOM = "2px solid rgba(255,255,255,0.5)";
+const CLICK_BORDER_BOTTOM = "2px solid white";
 
 function onSearchSubmit(e) {
   e.preventDefault();
@@ -17,6 +20,9 @@ function onSearchMouseOver(e) {
   e.preventDefault();
   searchInput.classList.remove("hidden");
   searchItag.classList.add(ICON_CLASS_NAME);
+  if (!clicked) {
+    searchForm.style.borderBottom = FOCUS_BORDER_BOTTOM;
+  }
   mouseOn = true;
 }
 function onSearchMouseOut(e) {
@@ -24,14 +30,25 @@ function onSearchMouseOut(e) {
   mouseOn = false;
 }
 
+function onSearchClick(e) {
+  e.preventDefault();
+  searchForm.style.borderBottom = CLICK_BORDER_BOTTOM;
+  clicked = true;
+}
+
 function onBodyClick(e) {
   e.preventDefault();
-  if (mouseOn == false && searchInput.value === "") {
-    searchInput.classList.add("hidden");
-    searchItag.classList.remove(ICON_CLASS_NAME);
+  if (mouseOn == false) {
+    if (searchInput.value === "") {
+      searchInput.classList.add("hidden");
+      searchItag.classList.remove(ICON_CLASS_NAME);
+    }
+    searchForm.style.borderBottom = "";
+    clicked = false;
   }
 }
 searchForm.addEventListener("submit", onSearchSubmit);
 searchForm.addEventListener("mouseover", onSearchMouseOver);
 searchForm.addEventListener("mouseout", onSearchMouseOut);
+searchForm.addEventListener("click", onSearchClick);
 body.addEventListener("click", onBodyClick);
